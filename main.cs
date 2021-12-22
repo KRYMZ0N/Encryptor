89568e10388c85f1948166da0a083d5e76494f99
KRYMZ0N <78245990+KRYMZ0N@users.noreply.github.com>
2021-12-22 13:19:49 +0000

    Added more functionality

--- main.cs
+++ main.cs
@@ -7,7 +7,7 @@ class Program {
     String Input = Console.ReadLine().ToLower();
 
 
-    for (int i = 0; i < Input.Length; i++) {
+    for (int I = 0; I < Input.Length; I++) {
       switch (Input) {
         case string a when a.Contains("a"):
         Input = Input.Replace("a", "u");
@@ -25,99 +25,99 @@ class Program {
         Input = Input.Replace("d", "o");
         break;
 
-        case string d when d.Contains("e"):
-        Input = Input.Replace("d", "o");
+        case string e when e.Contains("e"):
+        Input = Input.Replace("e", "p");
         break;
 
-        case string d when d.Contains("f"):
-        Input = Input.Replace("d", "o");
+        case string f when f.Contains("f"):
+        Input = Input.Replace("f", "n");
         break;
 
-        case string d when d.Contains("g"):
-        Input = Input.Replace("d", "o");
+        case string g when g.Contains("g"):
+        Input = Input.Replace("g", "l");
         break;
 
-        case string d when d.Contains("h"):
-        Input = Input.Replace("d", "o");
+        case string h when h.Contains("h"):
+        Input = Input.Replace("h", "g");
         break;
 
-        case string d when d.Contains("i"):
-        Input = Input.Replace("d", "o");
+        case string i when i.Contains("i"):
+        Input = Input.Replace("i", "q");
         break;
 
-        case string d when d.Contains("j"):
-        Input = Input.Replace("d", "o");
+        case string j when j.Contains("j"):
+        Input = Input.Replace("j", "w");
         break;
 
-        case string d when d.Contains("k"):
-        Input = Input.Replace("d", "o");
+        case string k when k.Contains("k"):
+        Input = Input.Replace("k", "h");
         break;
 
-        case string d when d.Contains("l"):
-        Input = Input.Replace("d", "o");
+        case string l when l.Contains("l"):
+        Input = Input.Replace("l", "z");
         break;
 
-        case string d when d.Contains("m"):
-        Input = Input.Replace("d", "o");
+        case string m when m.Contains("m"):
+        Input = Input.Replace("m", "a");
         break;
 
-        case string d when d.Contains("n):
-        Input = Input.Replace("d", "o");
+        case string n when n.Contains("n"):
+        Input = Input.Replace("n", "i");
         break;
 
-        case string d when d.Contains("o"):
-        Input = Input.Replace("d", "o");
+        case string o when o.Contains("o"):
+        Input = Input.Replace("o", "b");
         break;
 
-        case string d when d.Contains("p"):
-        Input = Input.Replace("d", "o");
+        case string p when p.Contains("p"):
+        Input = Input.Replace("p", "c");
         break;
 
-        case string d when d.Contains("q"):
-        Input = Input.Replace("d", "o");
+        case string q when q.Contains("q"):
+        Input = Input.Replace("q", "r");
         break;
 
-        case string d when d.Contains("r"):
-        Input = Input.Replace("d", "o");
+        case string r when r.Contains("r"):
+        Input = Input.Replace("r", "a");
         break;
 
-        case string d when d.Contains("s"):
-        Input = Input.Replace("d", "o");
+        case string s when s.Contains("s"):
+        Input = Input.Replace("s", "f");
         break;
 
-        case string d when d.Contains("t"):
-        Input = Input.Replace("d", "o");
+        case string t when t.Contains("t"):
+        Input = Input.Replace("t", "k");
         break;
 
-        case string d when d.Contains("u"):
-        Input = Input.Replace("d", "o");
+        case string u when u.Contains("u"):
+        Input = Input.Replace("u", "l");
         break;
 
-        case string d when d.Contains("v"):
-        Input = Input.Replace("d", "o");
+        case string v when v.Contains("v"):
+        Input = Input.Replace("v", "5");
         break;
 
-        case string d when d.Contains("w"):
-        Input = Input.Replace("d", "o");
+        case string w when w.Contains("w"):
+        Input = Input.Replace("w", "2");
         break;
 
-        case string d when d.Contains("x"):
-        Input = Input.Replace("d", "o");
+        case string x when x.Contains("x"):
+        Input = Input.Replace("x", "3");
         break;
 
-        case string d when d.Contains("y"):
-        Input = Input.Replace("d", "o");
+        case string y when y.Contains("y"):
+        Input = Input.Replace("y", "7");
         break;
 
-        case string d when d.Contains("z"):
-        Input = Input.Replace("d", "o");
+        case string z when z.Contains("z"):
+        Input = Input.Replace("z", "9");
         break;
 
         default:
-        Console.WriteLine("Fuck Off");
+        Console.WriteLine("not work");
         break;
       }
-      Console.WriteLine("added 1 to i. i now equals: " + i);
+      Console.WriteLine("added 1 to i. i now equals: " + I);
     }
 
   Console.WriteLine(Input);
